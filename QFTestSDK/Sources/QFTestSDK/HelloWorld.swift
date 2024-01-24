22bf8cb6509827272113ab36f6fd61504fa357f4
BstaffanUB <37052881+BstaffanUB@users.noreply.github.com>
2024-01-24 11:44:07 -0500

    Add files via upload

---
+++ QFTestSDK/Sources/QFTestSDK/HelloWorld.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Benjamin Staffan on 1/24/24.
+//
+
+import Foundation
+
+class HelloWorld{
+    func printWorld(){
+        debugPrint("HelloWorld")
+    }
+}
---
+++ QFTestSDK/Tests/QFTestSDKTests/QFTestSDKTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import QFTestSDK
+
+final class QFTestSDKTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
